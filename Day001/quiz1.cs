431f03dcf839cbf7766aeb13d8bb33bc7b5237d7
gubean9 <127818787+gubean9@users.noreply.github.com>
2023-06-27 16:38:20 +0900

    몸무게
    quiz1
    Q : 최적의 몸무게 구하기
    => 입력 키 - 110 => 이상적 몸무게 출력

---
+++ Day001/quiz1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tall;
+            tall = int.Parse(Console.ReadLine());
+
+            int result = tall - 110;
+
+            Console.WriteLine(result);
+        }
+    }
+}
